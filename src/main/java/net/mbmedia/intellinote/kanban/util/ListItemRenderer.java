99497b0f0615ad6df0a02bc23c598d05fc2cbf2b
Max <max@mb-media.net>
2022-11-07 20:09:28 +0100

    set Border to listitems

--- src/main/java/net/mbmedia/intellinote/kanban/util/ListItemRenderer.java
+++ src/main/java/net/mbmedia/intellinote/kanban/util/ListItemRenderer.java
@@ -1,6 +1,8 @@
 package net.mbmedia.intellinote.kanban.util;
 
 import javax.swing.*;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
 public class ListItemRenderer extends JTextArea implements ListCellRenderer<String> {
@@ -9,6 +11,12 @@ public class ListItemRenderer extends JTextArea implements ListCellRenderer<Stri
     public Component getListCellRendererComponent(JList<? extends String> list, String value, int index, boolean isSelected, boolean cellHasFocus) {
         setTabSize(1);
         setText(value);
+        Color boarderColor = new Color(242, 242, 242);
+        CompoundBorder border = BorderFactory.createCompoundBorder(
+                new EmptyBorder(0, 0, 8, 0),
+                BorderFactory.createMatteBorder(0, 0, 2, 0, boarderColor)
+        );
+        setBorder(border);
         return this;
     }
 }
