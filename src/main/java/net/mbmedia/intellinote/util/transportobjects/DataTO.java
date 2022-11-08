b277ac139eb748822a582a85241b0e3ede6dccbb
Max <max@mb-media.net>
2022-11-08 11:48:05 +0100

    wrap data in base64

--- src/main/java/net/mbmedia/intellinote/util/transportobjects/DataTO.java
+++ src/main/java/net/mbmedia/intellinote/util/transportobjects/DataTO.java
@@ -1,6 +1,8 @@
 package net.mbmedia.intellinote.util.transportobjects;
 
 import java.io.Serializable;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 public class DataTO implements Serializable {
 
@@ -9,20 +11,42 @@ public class DataTO implements Serializable {
     private final String text;
 
     public DataTO(String[] todoItems, String[] doneItems, String text) {
-        this.todoItems = todoItems;
-        this.doneItems = doneItems;
-        this.text = text;
+        this.todoItems = encodeArray(todoItems);
+        this.doneItems = encodeArray(doneItems);
+        this.text = encode(text);
     }
 
     public String[] getTodoItems() {
-        return todoItems;
+        return decodeArray(todoItems);
     }
 
     public String[] getDoneItems() {
-        return doneItems;
+        return decodeArray(doneItems);
     }
 
     public String getText() {
-        return text;
+        return decode(text);
+    }
+
+    private String[] encodeArray(String[] toEncode){
+        for(int i=0; i<toEncode.length; i++){
+            toEncode[i] = encode(toEncode[i]);
+        }
+        return toEncode;
+    }
+
+    private String[] decodeArray(String[] toDecode){
+        for(int i=0; i<toDecode.length; i++){
+            toDecode[i] = decode(toDecode[i]);
+        }
+        return toDecode;
+    }
+
+    private String encode(String toEncode){
+        return Base64.getEncoder().encodeToString(toEncode.getBytes(StandardCharsets.UTF_8));
+    }
+
+    private String decode(String toDecode){
+        return new String(Base64.getDecoder().decode(toDecode));
     }
 }
